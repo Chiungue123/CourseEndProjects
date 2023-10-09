59450923ca823a3671c96165411d425696611c5d
RobertoStagg <stagg.roberto@gmail.com>
2023-10-09 10:34:43 -0700

    Populated Batch and Participant models

--- BatchManager/src/main/java/com/courseendprojects/model/Batch.java
+++ BatchManager/src/main/java/com/courseendprojects/model/Batch.java
@@ -1,5 +1,57 @@
 package com.courseendprojects.model;
 
+import java.util.*;
+
 public class Batch {
 
+	// Declare Variables
+	private Integer batchID;
+	private String name;
+	private ArrayList<Participant> participants;
+	private String timeSlot;
+	
+	
+	public Batch() {
+		
+	}
+	
+	public Batch(ArrayList<Participant> participants, String name, String timeSlot, Integer batchID) {
+		this.participants = participants;
+		this.name = name;
+		this.timeSlot = timeSlot;
+		this.batchID = batchID;
+	}
+
+	public ArrayList<Participant> getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(ArrayList<Participant> participants) {
+		this.participants = participants;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTimeSlot() {
+		return timeSlot;
+	}
+
+	public void setTimeSlot(String timeSlot) {
+		this.timeSlot = timeSlot;
+	}
+
+	public Integer getBatchID() {
+		return batchID;
+	}
+
+	public void setBatchID(Integer batchID) {
+		this.batchID = batchID;
+	}
+	
 }
--- BatchManager/src/main/java/com/courseendprojects/model/Participant.java
+++ BatchManager/src/main/java/com/courseendprojects/model/Participant.java
@@ -1,5 +1,59 @@
 package com.courseendprojects.model;
 
-public class Participant {
+import java.util.*;
 
+
+public class Participant {
+	
+	// Declare Variables
+	private String id;
+	private String name;
+	private ArrayList<Batch> batches;
+	private String email;
+	
+	// Constructors
+	Participant(){
+		
+	}
+	
+	Participant(String name, ArrayList<Batch> batches, String id, String email){
+		this.name = name;
+		this.batches = batches;
+		this.id = id;
+		this.email = email;
+	}
+	
+	// Getters & Setters
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public ArrayList<Batch> getBatches() {
+		return batches;
+	}
+	
+	public void setBatches(ArrayList<Batch> batches) {
+		this.batches = batches;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
+
